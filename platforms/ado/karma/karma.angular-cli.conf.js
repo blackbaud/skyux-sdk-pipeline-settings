1d859435560b14f8f308fc87a0b4c9381b753d32
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2020-12-07 10:48:49 -0500

    Minor updates to karma config

--- platforms/ado/karma/karma.angular-cli.conf.js
+++ platforms/ado/karma/karma.angular-cli.conf.js
@@ -3,33 +3,7 @@
 
 module.exports = function (config) {
   config.set({
-    basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
-    plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
-    ],
-    client: {
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
-    },
-    coverageIstanbulReporter: {
-      reports: ['html', 'lcovonly', 'text-summary'],
-      fixWebpackSourcePaths: true
-    },
-    reporters: ['progress', 'kjhtml'],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false,
-    restartOnFileChange: true
+    autoWatch: false,
+    singleRun: true
   });
-
-  require(
-    '@skyux-sdk/angular-builders/src/builders/karma/karma.default.conf.js'
-  )(config);
 };
