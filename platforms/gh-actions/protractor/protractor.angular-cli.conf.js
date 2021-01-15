1df9384c64fa85c684c2b0698bbada9100d9fadc
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2021-01-15 15:39:46 -0500

    Remove commented code

--- platforms/gh-actions/protractor/protractor.angular-cli.conf.js
+++ platforms/gh-actions/protractor/protractor.angular-cli.conf.js
@@ -5,7 +5,6 @@ exports.config = {
   capabilities: {
     browserName: 'chrome',
     chromeOptions: {
-      // binary: require('puppeteer').executablePath(),
       args: [
         '--disable-dev-shm-usage',
         '--disable-extensions',
