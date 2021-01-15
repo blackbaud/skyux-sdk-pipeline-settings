6c5c1f340e37b288768330dc5031fe913af0e6e7
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2021-01-15 09:09:15 -0500

    Use puppeteer for e2e browser

--- platforms/gh-actions/protractor/protractor.angular-cli.conf.js
+++ platforms/gh-actions/protractor/protractor.angular-cli.conf.js
@@ -2,4 +2,16 @@
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 
 exports.config = {
+  capabilities: {
+    browserName: 'chrome',
+    chromeOptions: {
+      binary: require('puppeteer').executablePath(),
+      args: [
+        '--disable-extensions',
+        '--disable-gpu',
+        '--headless',
+        '--no-sandbox'
+      ]
+    }
+  }
 };
