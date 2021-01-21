8dccb5e629a41fb7d5d8a9b5aa642bbac6ec9db4
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2021-01-21 10:57:23 -0500

    Fix visual config

--- platforms/gh-actions/protractor/protractor.angular-cli.conf.js
+++ platforms/gh-actions/protractor/protractor.angular-cli.conf.js
@@ -2,18 +2,17 @@
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 
 exports.config = {
-  capabilities: {
-    browserName: 'chrome',
-    chromeOptions: {
-      args: [
-        '--disable-dev-shm-usage',
-        '--disable-extensions',
-        '--disable-gpu',
-        '--headless',
-        '--ignore-certificate-errors',
-        '--no-sandbox',
-        '--start-maximized'
-      ]
+  params: {
+    skyuxVisualRegressionTestingConfig: {
+      compareScreenshot: {
+        basePath: 'screenshots-baseline',
+        diffPath: 'screenshots-diff',
+        createdPath: 'screenshots-created',
+        createdPathDiff: 'screenshots-created-diff',
+        baseline: true,
+        width: 1000,
+        height: 800
+      }
     }
   }
 };
