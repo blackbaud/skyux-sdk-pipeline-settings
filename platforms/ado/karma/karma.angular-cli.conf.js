bb7903fd83f732964f679b2030e2a51a927f9aaa
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2021-03-23 11:00:46 -0400

    Fix JUnit reporter

--- platforms/ado/karma/karma.angular-cli.conf.js
+++ platforms/ado/karma/karma.angular-cli.conf.js
@@ -2,15 +2,19 @@
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
 module.exports = function (config) {
+  config.reporters.push('junit');
+
   config.set({
     autoWatch: false,
     singleRun: true,
     coverageReporter: {
       reporters: [
         { type: 'text-summary' },
-        { type: 'cobertura' },
-        { type: 'junit' }
+        { type: 'cobertura' }
       ]
+    },
+    junitReporter: {
+      outputDir: require('path').join(process.cwd(), 'test-results')
     }
   });
 };
