fbf0d32d3df5adb05fbb6ff1fda806822899c55f
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2021-08-10 11:25:44 -0400

    Update ADO karma configs

--- platforms/ado/karma/karma.angular-cli.conf.js
+++ platforms/ado/karma/karma.angular-cli.conf.js
@@ -1,35 +1,20 @@
-// Karma configuration file, see link for more information
-// https://karma-runner.github.io/1.0/config/configuration-file.html
-
+const applyDefaultConfig = require('../../shared/karma/karma.angular-cli.conf');
+const applyCodeCoverageThresholdConfig = require('../../utility/apply-code-coverage-threshold-config');
 const applyBrowserStackKarmaConfig = require('../../utility/apply-browserstack-karma-config');
 
 function applyJUnitConfig(config) {
   config.reporters.push('junit');
 
-  config.plugins.push(
-    require('karma-junit-reporter')
-  );
+  config.plugins.push(require('karma-junit-reporter'));
 
   config.junitReporter = {
-    outputDir: require('path').join(process.cwd(), 'test-results')
+    outputDir: require('path').join(process.cwd(), 'test-results'),
   };
 }
 
 module.exports = function (config) {
+  applyDefaultConfig(config);
   applyJUnitConfig(config);
-
-  config.set({
-    autoWatch: false,
-    singleRun: true,
-    coverageReporter: {
-      reporters: [
-        { type: 'text-summary' },
-        { type: 'cobertura' }
-      ]
-    }
-  });
-
-  if (process.env.SKY_UX_CODE_COVERAGE_BROWSER_SET) {
-    applyBrowserStackKarmaConfig(config);
-  }
+  applyCodeCoverageThresholdConfig(config);
+  applyBrowserStackKarmaConfig(config);
 };
