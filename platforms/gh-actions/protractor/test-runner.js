882967adc47f102caa865331ae546abfa2f1242a
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2021-08-17 08:51:14 -0400

    Create protractor test-runner.js

---
+++ platforms/gh-actions/protractor/test-runner.js
@@ -0,0 +1,32 @@
+const crossSpawn = require('cross-spawn');
+const minimist = require('minimist');
+
+const argv = minimist(process.argv.slice(2));
+
+function runE2eTests() {
+  try {
+    process.env.SKY_UX_PROTRACTOR_PROJECT_ROOT = argv['project-root'];
+
+    const result = crossSpawn.sync(
+      'npx',
+      [
+        '-p',
+        '@angular/cli',
+        'ng',
+        'e2e',
+        '--protractor-config=./node_modules/@skyux-sdk/pipeline-settings/platforms/ado/protractor/protractor.angular-cli.conf.js',
+      ],
+      { stdio: 'inherit', cwd: process.cwd() }
+    );
+
+    if (result.exit) {
+      console.log('The command `ng e2e` failed.');
+      process.exit(1);
+    }
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+}
+
+runE2eTests();
