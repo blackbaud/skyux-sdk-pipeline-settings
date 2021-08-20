11ccf2d6b32daab632958ea450d96460d9c4ae6c
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2021-08-20 14:33:26 -0400

    Remove karma-jasmine-html-reporter

--- platforms/shared/karma/karma.angular-cli.conf.js
+++ platforms/shared/karma/karma.angular-cli.conf.js
@@ -10,7 +10,6 @@ module.exports = function (config) {
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma'),
     ],
@@ -23,15 +22,12 @@ module.exports = function (config) {
       },
       clearContext: false, // leave Jasmine Spec Runner output visible in browser
     },
-    jasmineHtmlReporter: {
-      suppressAll: true, // removes the duplicated traces
-    },
     coverageReporter: {
       dir: require('path').join(process.cwd(), './coverage'), // Angular sets this to './coverage/project-name' by default.
       subdir: '.',
       reporters: [{ type: 'html' }, { type: 'text-summary' }],
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['progress'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
