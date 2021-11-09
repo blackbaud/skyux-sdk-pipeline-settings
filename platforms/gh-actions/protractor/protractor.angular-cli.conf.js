d4f00a8deba242c5f3fec47aa037eaf7fc866c35
Steve Brush <steve.brush@blackbaud.com>
2021-11-09 09:34:30 -0500

    Use BrowserStack for visual tests (#4)

--- platforms/gh-actions/protractor/protractor.angular-cli.conf.js
+++ platforms/gh-actions/protractor/protractor.angular-cli.conf.js
@@ -1,3 +1,10 @@
 const getConfig = require('../../shared/protractor/protractor.angular-cli.conf');
+const applyBrowserStackProtractorConfig = require('../../utility/apply-browserstack-protractor-config');
 
-exports.config = getConfig();
+const config = getConfig();
+
+if (process.env.BROWSER_STACK_USERNAME) {
+  applyBrowserStackProtractorConfig(config);
+}
+
+exports.config = config;
---
+++ platforms/utility/apply-browserstack-protractor-config.js
@@ -0,0 +1,58 @@
+const browserstack = require('browserstack-local');
+const mergeWith = require('lodash.mergewith');
+
+let bsLocal;
+
+function applyBrowserStackProtractorConfig(config) {
+  const browserStackKey = process.env.BROWSER_STACK_ACCESS_KEY;
+
+  const browserStackOverrides = {
+    browserstackUser: process.env.BROWSER_STACK_USERNAME,
+    browserstackKey: browserStackKey,
+    capabilities: {
+      browserName: 'Chrome',
+      build: process.env.BROWSER_STACK_BUILD_ID,
+      name: 'ng e2e',
+      os: 'Windows',
+      os_version: '10',
+      project: process.env.BROWSER_STACK_PROJECT,
+      'browserstack.debug': 'true',
+      'browserstack.local': 'true',
+    },
+    directConnect: false,
+
+    beforeLaunch: function () {
+      console.log('Connecting to BrowserStack Local...');
+      return new Promise(function (resolve, reject) {
+        bsLocal = new browserstack.Local();
+        bsLocal.start({ key: browserStackKey }, function (error) {
+          if (error) return reject(error);
+          console.log('Connected. Now testing...');
+
+          resolve();
+        });
+      });
+    },
+
+    // Code to stop browserstack local after end of test
+    afterLaunch: function () {
+      return new Promise(function (resolve) {
+        bsLocal.stop(resolve);
+      });
+    },
+  };
+
+  config = mergeWith(
+    config,
+    browserStackOverrides,
+    function (originalValue, overrideValue) {
+      if (Array.isArray(originalValue)) {
+        return overrideValue;
+      }
+    }
+  );
+
+  return config;
+}
+
+module.exports = applyBrowserStackProtractorConfig;
